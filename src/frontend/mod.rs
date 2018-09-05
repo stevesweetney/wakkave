d9067f4d3da780f28bc928c7f4bbb50bc3daebef
Steve Sweetney <stevesweetney@gmail.com>
2018-09-05 15:50:04 -0400

    Update protocol service to match protocol schema. Add corresponding rust types for structs in the protocol schema.

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -1,5 +1,63 @@
+use protocol_capnp::{post as Post_P, Vote as Vote_P};
+
 pub mod components;
 pub mod routes;
 pub mod services;
 
 pub const SESSION_TOKEN: &str = "SessionToken";
+
+pub enum WsMessage {
+    Login,
+    Logout,
+    FetchPosts,
+    CreatePost,
+    UserVote,
+    InvalidPosts,
+    NewPost,
+    UpdateUsers,
+}
+
+pub struct User {
+    id: i32,
+    username: String,
+    karma: i32,
+    streak: i16,
+}
+
+pub struct Post {
+    id: i32,
+    content: String,
+    valid: bool,
+    vote: Vote,
+    userId: i32,
+}
+
+pub enum Vote {
+    Up,
+    None,
+    Down,
+}
+
+impl Into<Vote_P> for Vote {
+    fn into(self) -> Vote_P {
+        match self {
+            Vote::None => Vote_P::None,
+            Vote::Up => Vote_P::Up,
+            Vote::Down => Vote_P::Down,
+        }
+    }
+}
+
+impl From<Vote_P> for Vote {
+    fn from(v: Vote_P) -> Self {
+        match v {
+            Vote_P::None => Vote::None,
+            Vote_P::Up => Vote::Up,
+            Vote_P::Down => Vote::Down,
+        }
+    }
+}
+
+#[derive(Debug, Fail)]
+#[fail(display = "Invalid Request")]
+pub struct InvalidRequest;
--- src/frontend/services/protocol.rs
+++ src/frontend/services/protocol.rs
@@ -1,10 +1,13 @@
+use std::collections::HashSet;
+
 use capnp::{
     message::{Builder, HeapAllocator, ReaderOptions},
     serialize_packed,
 };
-use protocol_capnp::{request, response};
+use protocol_capnp::{post as Post_P, request, response, update, Vote as Vote_P};
 
 use failure::Error;
+use frontend::{Post, User, Vote, WsMessage};
 
 #[derive(Debug, Fail)]
 pub enum ProtocolError {
@@ -30,6 +33,32 @@ impl ProtocolService {
 
         serialize_packed::write_message(&mut self.data, &self.builder)?;
         Ok(&self.data)
+    }
+
+    pub fn which_message(mut data: &[u8]) -> Result<WsMessage, Error> {
+        let reader = serialize_packed::read_message(&mut data, ReaderOptions::new())?;
+        let response = reader.get_root::<response::Reader>();
+
+        // Check to see first to see if the data is a response message or an update
+        let res = match response {
+            Ok(response) => match response.which()? {
+                response::Login(_) => WsMessage::Login,
+                response::Logout(_) => WsMessage::Logout,
+                response::FetchPosts(_) => WsMessage::FetchPosts,
+                response::CreatePost(_) => WsMessage::CreatePost,
+                response::UserVote(_) => WsMessage::UserVote,
+            },
+            Err(_) => {
+                let update = reader.get_root::<update::Reader>()?;
+                match update.which()? {
+                    update::Invalid(_) => WsMessage::InvalidPosts,
+                    update::NewPost(_) => WsMessage::NewPost,
+                    update::Users(_) => WsMessage::UpdateUsers,
+                }
+            }
+        };
+
+        Ok(res)
     }
 
     pub fn write_request_login_credentials(
@@ -59,6 +88,16 @@ impl ProtocolService {
         self.write()
     }
 
+    pub fn write_request_logout_token(&mut self, token: &str) -> Result<&[u8], Error> {
+        {
+            self.builder
+                .init_root::<request::Builder>()
+                .set_logout(token);
+        }
+
+        self.write()
+    }
+
     pub fn write_request_registration(
         &mut self,
         name: &str,
@@ -72,6 +111,58 @@ impl ProtocolService {
             registration.set_username(name);
             registration.set_password(password);
         }
+        self.write()
+    }
+
+    pub fn write_request_fetch_posts(&mut self, token: &str) -> Result<&[u8], Error> {
+        {
+            self.builder
+                .init_root::<request::Builder>()
+                .set_fetch_posts(token);
+        }
+
+        self.write()
+    }
+
+    pub fn write_request_create_post(
+        &mut self,
+        token: &str,
+        content: &str,
+    ) -> Result<&[u8], Error> {
+        {
+            let mut req = self
+                .builder
+                .init_root::<request::Builder>()
+                .init_create_post();
+            req.set_token(token);
+            req.set_content(content);
+        }
+
+        self.write()
+    }
+
+    pub fn write_request_user_vote(
+        &mut self,
+        token: &str,
+        post_id: i32,
+        vote: Vote,
+    ) -> Result<&[u8], Error> {
+        {
+            let mut req = self
+                .builder
+                .init_root::<request::Builder>()
+                .init_user_vote();
+            let vote = match vote {
+                Vote::Up => Vote_P::Up,
+                Vote::Down => Vote_P::Down,
+                Vote::None => Vote_P::None,
+            };
+
+            req.set_token(token);
+            req.set_post_id(post_id);
+            req.set_vote(vote);
+        }
+
         self.write()
     }
 
@@ -90,6 +181,160 @@ impl ProtocolService {
                     description: error?.to_owned(),
                 })),
             },
+            _ => Ok(None),
+        }
+    }
+
+    pub fn read_response_logout(&self, mut data: &[u8]) -> Result<Option<()>, Error> {
+        let reader = serialize_packed::read_message(&mut data, ReaderOptions::new())?;
+        let response = reader.get_root::<response::Reader>()?;
+
+        match response.which()? {
+            response::Logout(data) => match data.which()? {
+                response::logout::Success(_) => Ok(Some(())),
+                response::logout::Error(error) => Err(Error::from(ProtocolError::Response {
+                    description: error?.to_owned(),
+                })),
+            },
+            _ => Ok(None),
+        }
+    }
+
+    pub fn read_response_fetch_posts(
+        &self,
+        mut data: &[u8],
+    ) -> Result<Option<(String, Vec<Post>)>, Error> {
+        let reader = serialize_packed::read_message(&mut data, ReaderOptions::new())?;
+        let response = reader.get_root::<response::Reader>()?;
+
+        match response.which()? {
+            response::FetchPosts(data) => match data.which()? {
+                response::fetch_posts::Success(data) => {
+                    let token = data.get_token()?.to_string();
+                    let mut posts = Vec::<Post>::new();
+
+                    for post in data.get_posts()?.iter() {
+                        posts.push(Post {
+                            id: post.get_id(),
+                            content: post.get_content()?.to_string(),
+                            valid: post.get_valid(),
+                            vote: post.get_vote()?.into(),
+                            userId: post.get_user_id(),
+                        })
+                    }
+
+                    Ok(Some((token, posts)))
+                }
+                response::fetch_posts::Error(error) => Err(Error::from(ProtocolError::Response {
+                    description: error?.to_owned(),
+                })),
+            },
+            _ => Ok(None),
+        }
+    }
+
+    pub fn read_response_create_post(
+        &self,
+        mut data: &[u8],
+    ) -> Result<Option<(String, Post)>, Error> {
+        let reader = serialize_packed::read_message(&mut data, ReaderOptions::new())?;
+        let response = reader.get_root::<response::Reader>()?;
+
+        match response.which()? {
+            response::CreatePost(data) => match data.which()? {
+                response::create_post::Success(data) => {
+                    let token = data.get_token()?.to_string();
+                    let post = data.get_post()?;
+
+                    let post = Post {
+                        id: post.get_id(),
+                        content: post.get_content()?.to_string(),
+                        valid: post.get_valid(),
+                        vote: post.get_vote()?.into(),
+                        userId: post.get_user_id(),
+                    };
+                    Ok(Some((token, post)))
+                }
+                response::create_post::Error(error) => Err(Error::from(ProtocolError::Response {
+                    description: error?.to_owned(),
+                })),
+            },
+            _ => Ok(None),
+        }
+    }
+
+    pub fn read_request_user_vote(&self, mut data: &[u8]) -> Result<Option<(String)>, Error> {
+        let reader = serialize_packed::read_message(&mut data, ReaderOptions::new())?;
+        let response = reader.get_root::<response::Reader>()?;
+
+        match response.which()? {
+            response::UserVote(data) => match data.which()? {
+                response::user_vote::Success(data) => Ok(Some(data?.to_string())),
+                response::user_vote::Error(error) => Err(Error::from(ProtocolError::Response {
+                    description: error?.to_owned(),
+                })),
+            },
+            _ => Ok(None),
+        }
+    }
+
+    pub fn read_update_new_post(&self, mut data: &[u8]) -> Result<Option<Post>, Error> {
+        let reader = serialize_packed::read_message(&mut data, ReaderOptions::new())?;
+        let update = reader.get_root::<update::Reader>()?;
+
+        match update.which()? {
+            update::NewPost(data) => {
+                let post = data?;
+                let post = Post {
+                    id: post.get_id(),
+                    content: post.get_content()?.to_string(),
+                    valid: post.get_valid(),
+                    vote: post.get_vote()?.into(),
+                    userId: post.get_user_id(),
+                };
+
+                Ok(Some(post))
+            }
+            _ => Ok(None),
+        }
+    }
+
+    pub fn read_update_invalid(&self, mut data: &[u8]) -> Result<Option<HashSet<i32>>, Error> {
+        let reader = serialize_packed::read_message(&mut data, ReaderOptions::new())?;
+        let update = reader.get_root::<update::Reader>()?;
+
+        match update.which()? {
+            update::Invalid(data) => {
+                let post_ids = data?;
+                let mut set = HashSet::new();
+                for id in post_ids.iter() {
+                    set.insert(id);
+                }
+
+                Ok(Some(set))
+            }
+            _ => Ok(None),
+        }
+    }
+
+    pub fn read_update_users(&self, mut data: &[u8]) -> Result<Option<Vec<User>>, Error> {
+        let reader = serialize_packed::read_message(&mut data, ReaderOptions::new())?;
+        let update = reader.get_root::<update::Reader>()?;
+
+        match update.which()? {
+            update::Users(data) => {
+                let mut users = Vec::new();
+                for user in data?.iter() {
+                    users.push(User {
+                        id: user.get_id(),
+                        username: user.get_username()?.to_string(),
+                        karma: user.get_karma(),
+                        streak: user.get_streak(),
+                    });
+                }
+
+                Ok(Some(users))
+            }
             _ => Ok(None),
         }
     }
